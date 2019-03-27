5e4ec79b2caec91e63e59e8b9b32adf6a5d4900a
derekhof <34960357+derekhof@users.noreply.github.com>
2019-03-27 09:08:16 +0100

    Update GrapyClasses.py

--- GrapyClasses.py
+++ GrapyClasses.py
@@ -1,5 +1,6 @@
 # imports for de functie SinglePageScraper
 import requests
+from multiprocessing import Pool
 from bs4 import BeautifulSoup
 from bs4 import Comment
 import json
@@ -11,23 +12,13 @@ class SinglePageScraper:
         self.keywords = keywords
         self.string_finding_part1 = string_finding_part_1
         self.string_finding_part2 = string_finding_part_2
-        self.url = None
 
-
-
-    def setUrl(self):
-        self.url = input("Set URL:")
-
-
-    def showKeywords(self):
-        for keyword in self.keywords:
-            print("\n" + keyword)
 
     # The function scrapes a specific url and checks if there is a match with the defined keywords
-    def runSinglePageScraper(self):
+    def runSinglePageScraper(self, url):
 
         try:
-            result = requests.get(self.url)
+            result = requests.get(url)
 
             # check for status HTTP status code
             if result.status_code == 200:
@@ -39,20 +30,17 @@ class SinglePageScraper:
 
                 positive_keywords = []
 
-
                 for comment in comments:
 
                     for keyword in self.keywords:
-
-                        # temp test
-                        print(comment)
-                        print("===========")
-                        comment.extract()
+                        #
+                        # # temp test
+                        # print(comment)
+                        # print("===========")
+                        # comment.extract()
                         if keyword in comment:
-
                             # add positive match to results
                             positive_keywords.append(keyword)
-
 
                 # remove duplicatie keyword matches
                 positive_keywords = list(set(positive_keywords))
@@ -68,52 +56,45 @@ class SinglePageScraper:
 
                     counter = counter + 1
 
-                # print(string_finding_part_1)
-                # print(string_finding_part_2)
-                # print(url)
-                # print(string_positive_keywords)
-
-
                 # return results
                 if counter > 0:
-                    self.finding = Finding(self.url, self.keywords)
-                    print( self.string_finding_part1 + self.url +  self.string_finding_part2 + string_positive_keywords)
+                    self.finding = Finding(url, self.keywords)
+                    print(self.string_finding_part1 + url + self.string_finding_part2 + string_positive_keywords)
+
+                    return self.finding
 
                 else:
                     print("NO MATCHES")
 
             else:
-                print("HTTP request failed, HTTP reponse status code: " + result.status_code)
+                print("HTTP request failed, HTTP reponse status code: " + str(result.status_code))
 
         except:
-            print("The URL is not valid or not set! set a valid URL. Use the following structure: http(s)://xxxx.xx ")
-
-    def showURL(self):
-        return print(self.url)
-
-    def writeToFileExchange(self, path, fileName):
-
-        try:
-
-            filePathNameWExt = './' + path + '/' + fileName + '.json'
-            with open(filePathNameWExt, 'w') as fp:
-                json.dump(self.finding.resultToDict(), fp)
-
-                print("Finding are succesfully exported")
-
-        except:
-            print("There are no findings")
+                print("The URL is not valid or not set! set a valid URL. Use the following structure: http(s)://xxxx.xx ")
 
 
+def writeToFileExchange(self, path, fileName):
+    try:
 
+        filePathNameWExt = './' + path + '/' + fileName + '.json'
+        with open(filePathNameWExt, 'w') as fp:
 
+            json.dump(self.finding.resultToDict(), fp)
+
+            print("Finding are succesfully exported")
+
+    except:
+        print("There are no findings")
+
+####################################################
+############ Findings handler class ################
+####################################################
 
 class Finding:
     def __init__(self, url, keywords):
         self.url = url
         self.keywords = keywords
         self.dict = None
-
 
     def resultToDict(self):
         self.dict = {}
@@ -123,8 +104,7 @@ class Finding:
         return self.dict
 
 
-
-
+# Configuration handler class
 class IniConfig:
 
     def __init__(self):
@@ -137,11 +117,11 @@ class IniConfig:
                 self.string_finding_part1 = self.config["DEFAULT_FINDING_STRING_PART_1"]
                 self.string_finding_part2 = self.config["DEFAULT_FINDING_STRING_PART_2"]
                 self.keywords = self.config["KEYWORDS"]
+                self.url = self.config["DEFAULT_URL"]
                 self.status = "SUCCEED"
         except:
 
             self.status = "FAILED"
-
 
     def addKeyword(self):
 
@@ -156,13 +136,11 @@ class IniConfig:
 
         # only add keyword if it is does already exists
         if doubleKeyword == False:
-                self.keywords.append(newKeyword)
-                self.config["KEYWORDS"] = self.keywords
+            self.keywords.append(newKeyword)
+            self.config["KEYWORDS"] = self.keywords
 
-                with open('config.json', 'w') as f:
-                    json.dump(self.config, f)
-
-
+            with open('config.json', 'w') as f:
+                json.dump(self.config, f)
 
     def deleteKeyword(self):
 
@@ -172,18 +150,102 @@ class IniConfig:
         # Check if keyword already exist
         for keyword in self.keywords:
             if keyword == deleteKeyword:
-
                 KeywordExists = True
 
         # only add keyword if it is does already exists
         if KeywordExists == True:
-                self.keywords.remove(deleteKeyword)
-                self.config["KEYWORDS"] = self.keywords
+            self.keywords.remove(deleteKeyword)
+            self.config["KEYWORDS"] = self.keywords
 
-                with open('config.json', 'w') as f:
-                    json.dump(self.config, f)
+            with open('config.json', 'w') as f:
+                json.dump(self.config, f)
 
-                print("Keyword deleted")
+            print("Keyword deleted")
 
         else:
             print("Keyword does not exists")
+
+
+    def setUrl(self):
+            self.url = input("Set URL:")
+
+    def showKeywords(self):
+            for keyword in self.keywords:
+                print("\n" + keyword)
+
+    def showURL(self):
+        return print(self.url)
+
+
+class Crawler:
+
+    def handleLocalLinks(self, url, link):
+        if link != None:
+            if link.startswith('/'):
+                return ''.join([url, link])
+            else:
+                return link
+
+    def getLinks(self, url):
+        try:
+            resp = requests.get(url)
+            soup = BeautifulSoup(resp.text, 'lxml')
+            body = soup.body
+
+            links = [link.get('href') for link in body.find_all('a')]
+            links = [self.handleLocalLinks(url, link) for link in links]
+            return links
+
+        except TypeError as e:
+            print(e)
+            print('Got a TypeError, probably got a None that we tried to iterate over')
+            return []
+        except IndexError as e:
+            print(e)
+            print('We probably did not find any useful links, returning empty list')
+            return []
+        except AttributeError as e:
+            print(e)
+            print('Likely got None for links, so we are throwing this')
+            return []
+        except Exception as e:
+            print(str(e))
+            # log this error
+            return []
+
+    def removeDuplicates(self, data):
+        seen = set()
+        seen_add = seen.add
+        return [x for x in data if not (x in seen or seen_add(x))]
+
+    def createFqdn(self, url, link):
+        if link != None:
+            if not link.startswith('http'):
+                return '/'.join([url, link])
+            else:
+                return link
+
+    def runCrawler(self, url, config):
+        data = self.getLinks(url)
+        if None in data:
+            data.remove(None)
+        data = self.removeDuplicates(data)
+        data = [self.createFqdn(url, link) for link in data]
+
+        # use singeplagescraper
+        singlePageScraper = SinglePageScraper(config.keywords, config.string_finding_part1, config.string_finding_part2)
+
+        findings = []
+
+        for link in data:
+            finding = singlePageScraper.runSinglePageScraper(link)
+
+            if finding != None:
+                findings.append(finding)
+
+
+
+        for finding in findings:
+            print(finding.resultToDict())
+
+
